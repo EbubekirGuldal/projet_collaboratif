447f37fdfee7016aaa220393f46c14096739a298
Ebubekir Guldal <ebubekir57450@gmail.com>
2026-02-25 09:14:26 +0100

    Récupération de la page d'accueil

--- migrations/Version20260225080601.php
+++ migrations/Version20260225080601.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260224075716 extends AbstractMigration
+final class Version20260225080601 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,6 +23,12 @@ final class Version20260224075716 extends AbstractMigration
         $this->addSql('CREATE TABLE user_favorites (user_id INT NOT NULL, resource_id INT NOT NULL, INDEX IDX_E489ED11A76ED395 (user_id), INDEX IDX_E489ED1189329D25 (resource_id), PRIMARY KEY(user_id, resource_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_favorites ADD CONSTRAINT FK_E489ED11A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_favorites ADD CONSTRAINT FK_E489ED1189329D25 FOREIGN KEY (resource_id) REFERENCES resource (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment ADD user_id INT NOT NULL, DROP status, DROP updated_at, CHANGE content content LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_9474526CA76ED395 ON comment (user_id)');
+        $this->addSql('ALTER TABLE resource ADD user_id INT DEFAULT NULL, ADD likes_count INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE resource ADD CONSTRAINT FK_BC91F416A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_BC91F416A76ED395 ON resource (user_id)');
     }
 
     public function down(Schema $schema): void
@@ -31,5 +37,11 @@ final class Version20260224075716 extends AbstractMigration
         $this->addSql('ALTER TABLE user_favorites DROP FOREIGN KEY FK_E489ED11A76ED395');
         $this->addSql('ALTER TABLE user_favorites DROP FOREIGN KEY FK_E489ED1189329D25');
         $this->addSql('DROP TABLE user_favorites');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
+        $this->addSql('DROP INDEX IDX_9474526CA76ED395 ON comment');
+        $this->addSql('ALTER TABLE comment ADD status VARCHAR(255) DEFAULT \'En attente\' NOT NULL, ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', DROP user_id, CHANGE content content LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE resource DROP FOREIGN KEY FK_BC91F416A76ED395');
+        $this->addSql('DROP INDEX IDX_BC91F416A76ED395 ON resource');
+        $this->addSql('ALTER TABLE resource DROP user_id, DROP likes_count');
     }
 }
